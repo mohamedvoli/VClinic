7d7a231da745389be5ceecb5224f8352677a828d
voli <mohamed.ahmed8902@gmail.com>
2025-01-08 20:24:15 +0200

    Update

--- VClinic.API/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VClinic.API.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
